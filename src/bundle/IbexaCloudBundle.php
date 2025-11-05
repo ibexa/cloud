7e6946e0635835a2cdfa4a45b72b1991ff3f1012
Bartek Wajda <bartlomiej.wajda@ibexa.co>
2025-11-05 01:07:15 +0100

    IBX-10764: Add test env

--- src/bundle/IbexaCloudBundle.php
+++ src/bundle/IbexaCloudBundle.php
@@ -16,6 +16,7 @@ final class IbexaCloudBundle extends Bundle
     public function boot(): void
     {
         $envVars = (new UpsunEnvVarLoader())->loadEnvVars();
+        $envVars = ['TEST_ENV_VAR' => 'test'];
 
         foreach ($envVars as $name => $value) {
             $value = (string) $value;
