572e4a501ffb6bd8788c8f61df70e4a0bb071610
Bartek Wajda <bartlomiej.wajda@ibexa.co>
2025-11-28 15:02:59 +0100

    IBX-10764: Added container configuration

--- src/bundle/DependencyInjection/IbexaCloudExtension.php
+++ src/bundle/DependencyInjection/IbexaCloudExtension.php
@@ -36,6 +36,7 @@ final class IbexaCloudExtension extends Extension implements PrependExtensionInt
 
     public function prepend(ContainerBuilder $container): void
     {
+        $this->configureUpsunSetup($container);
         $this->prependDefaultConfiguration($container);
         $this->prependJMSTranslation($container);
 
@@ -83,5 +84,102 @@ final class IbexaCloudExtension extends Extension implements PrependExtensionInt
     {
         return $container->hasParameter('ibexa.behat.browser.enabled')
             && true === $container->getParameter('ibexa.behat.browser.enabled');
+    }
+
+    private function configureUpsunSetup(ContainerBuilder $container): void
+    {
+        $envVars = (new UpsunEnvVarLoader())->loadEnvVars();
+
+        if ($envVars === []) {
+            return;
+        }
+
+        $projectDir = $container->getParameter('kernel.project_dir');
+
+        // Map environment variables to container parameters
+        $this->setParametersFromEnvVars($container, $envVars, $projectDir);
+
+        // Load additional YAML configuration files based on enabled services
+        $this->loadServiceConfigurations($container, $envVars, $projectDir);
+    }
+
+    /**
+     * @param array<string, string> $envVars
+     */
+    private function setParametersFromEnvVars(
+        ContainerBuilder $container,
+        array $envVars,
+        string $projectDir
+    ): void {
+        $parameterMapping = [
+            'DFS_DATABASE_DRIVER' => 'dfs_database_driver',
+            'DFS_DATABASE_URL' => 'dfs_database_url',
+            'DFS_DATABASE_CHARSET' => 'dfs_database_charset',
+            'DFS_DATABASE_COLLATION' => 'dfs_database_collation',
+            'CACHE_POOL' => 'cache_pool',
+            'CACHE_DSN' => 'cache_dsn',
+            'SESSION_HANDLER_ID' => 'ibexa.session.handler_id',
+            'SESSION_SAVE_PATH' => 'ibexa.session.save_path',
+            'SEARCH_ENGINE' => 'search_engine',
+            'SOLR_DSN' => 'solr_dsn',
+            'SOLR_CORE' => 'solr_core',
+            'ELASTICSEARCH_DSN' => 'elasticsearch_dsn',
+            'HTTPCACHE_PURGE_TYPE' => ['purge_type', 'ibexa.http_cache.purge_type'],
+            'HTTPCACHE_PURGE_SERVER' => 'purge_server',
+            'HTTPCACHE_VARNISH_INVALIDATE_TOKEN' => 'varnish_invalidate_token',
+        ];
+
+        foreach ($envVars as $envKey => $value) {
+            if (!isset($parameterMapping[$envKey])) {
+                continue;
+            }
+
+            $parameters = (array) $parameterMapping[$envKey];
+            foreach ($parameters as $parameterName) {
+                $container->setParameter($parameterName, $value);
+            }
+        }
+
+        // Handle DFS_NFS_PATH specially - convert from relative to absolute path
+        if (isset($envVars['DFS_NFS_PATH'])) {
+            $absolutePath = sprintf('%s/%s', $projectDir, $envVars['DFS_NFS_PATH']);
+            $container->setParameter('dfs_nfs_path', $absolutePath);
+        }
+    }
+
+    /**
+     * @param array<string, string> $envVars
+     */
+    private function loadServiceConfigurations(
+        ContainerBuilder $container,
+        array $envVars,
+        string $projectDir
+    ): void {
+        // Load DFS configuration if DFS is enabled
+        if (isset($envVars['DFS_NFS_PATH'])) {
+            $loader = new YamlFileLoader(
+                $container,
+                new FileLocator($projectDir . '/config/packages/dfs')
+            );
+            $loader->load('dfs.yaml');
+        }
+
+        // Load cache configuration based on cache type
+        if (isset($envVars['CACHE_POOL'])) {
+            $cacheType = $envVars['CACHE_POOL'];
+            $configFile = match ($cacheType) {
+                'cache.redis' => 'cache.redis.yaml',
+                'cache.memcached' => 'cache.memcached.yaml',
+                default => null,
+            };
+
+            if ($configFile !== null) {
+                $loader = new YamlFileLoader(
+                    $container,
+                    new FileLocator($projectDir . '/config/packages/cache_pool')
+                );
+                $loader->load($configFile);
+            }
+        }
     }
 }
